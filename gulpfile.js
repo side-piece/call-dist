2ad62d157d3a4f1e566bc50e9dfa9a9c19656c71
Colton Shields <colton@bettrnet.com>
2016-07-28 23:26:28 -0600

    testing deploy

--- gulpfile.js
+++ gulpfile.js
@@ -27,3 +27,8 @@ fs.readdirSync('./gulp').filter(function(file) {
 gulp.task('default', ['clean'], function () {
   gulp.start('build');
 });
+
+
+gulp.task('heroku:production', ['build'], function(){
+  console.log('heroku production');
+});
