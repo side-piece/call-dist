19406c05580b906803f32d3a056025c7c1d94169
Colton Shields <colton@bettrnet.com>
2016-08-03 20:43:40 -0600

    posting to firebase answerer now

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,43 +6,13 @@
 		.controller('MainController', MainController);
 
 	/** @ngInject */
-	function MainController($scope, $location, anchorSmoothScroll, $firebaseObject, $log) {
+	function MainController($scope, $location,anchorSmoothScroll, $http, $firebaseObject, $log) {
 		var vm = this;
-		vm.phone = ""
-
-		firebase.auth().onAuthStateChanged(function (user) {
-			console.log('This is the user',user);
-			if (user) {
-				// User is signed in.
-				var isAnonymous = user.isAnonymous;
-				var uid = user.uid;
-				// ...
-			} else {
-				console.log('user is signed out ');
-				// User is signed out.
-				// ...
-			}
-			// ...
-		});
-
-		firebase.auth().signInAnonymously().catch(function (error) {
-			// Handle Errors here.
-			var errorCode = error.code;
-			console.log(errorCode);
-			var errorMessage = error.message;
-			console.log(errorMessage);
-			// ...
-		});
-
-		var ref = new Firebase("https://call-distributor-dev.firebaseio.com/");
-		$scope.firebase = $firebaseObject(ref);
-
-		console.log('PHONZZZZZ', $scope.firebase);
-
-
-		//
-		// // download the data into a local object
-		// $scope.data = $firebaseObject(ref);
+		vm.phone = '';
+		var answerer = {
+				"availabilitity": false,
+				"phoneNumber": vm.phone
+		}
 
 		vm.gotoElement = function (eID) {
 			$location.hash('signup');
@@ -50,16 +20,15 @@
 		};
 
 		vm.submitNumber = function () {
-
-			$log.debug("posting this number to our DB birches", vm.phone);
-			$scope.firebase.phonenumbers = vm.phone;
-			$scope.firebase.$save().then(function (ref) {
-				console.log('phone numz saved');
-				console.log(ref);
-			}, function (error) {
-				$log.error("Error:", error);
+			$http({
+				method: 'POST',
+				url: 'https://call-distributor-dev.firebaseio.com/answerers.json',
+				data: answerer
+			}).then(function successCallback(response) {
+				$log.debug(response);
+			}, function errorCallback(response) {
+				$log.debug(response);
 			});
-
 
 		}
 
