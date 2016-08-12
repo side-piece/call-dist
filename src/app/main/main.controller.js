d0890dfbf4e394b6d8b27454bfa2c61d268274c3
troylelandshields <troy.leland.shields@gmail.com>
2016-08-11 20:59:24 -0600

    Use alpha queue instead of posting directly to firebase.

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,3 +1,17 @@
+
+//All of this firebase stuff should be abstracted in a service/factory
+//TODO dont hardcode this sensitive data anywhere and especially not in the middle of the friggin' controller'
+var config = {
+	apiKey: "AIzaSyANdTshfeci78-VI-m-NaGHu4BWMIMSn0U",
+	authDomain: "call-distributor-dev.firebaseapp.com",
+	databaseURL: "https://call-distributor-dev.firebaseio.com",
+	storageBucket: "call-distributor-dev.appspot.com",
+};
+firebase.initializeApp(config, "call-dist");
+var tasksRef = firebase.database().ref().child("queue").child("tasks");
+var newNumberSvc = new AlphaQueue(tasksRef, ["new_answerer"]); //This specifically should be injected and/or wrapped
+
+
 (function () {
 	'use strict';
 
@@ -6,7 +20,7 @@
 		.controller('MainController', MainController);
 
 	/** @ngInject */
-	function MainController($scope, $location, anchorSmoothScroll, $http, $firebaseObject, $log) {
+	function MainController($scope, $location, anchorSmoothScroll, $firebase, $log) {
 		var vm = this;
 		vm.phone = '';
 
@@ -18,28 +32,24 @@
 		};
 
 		vm.submitNumber = function () {
+
+
 			//remove the -'s and ()'s
 			var phone = vm.phone.replace(/[^+\d]+/g, "");
 			var answerer = {
 				phoneNumber: 1 + phone,
-				_state: "new_answerer"
 			}
 
 			$log.debug('posting this: ', answerer);
 
-			$http({
-				method: 'POST',
-				//post data to the queue tasks to have the new_answerer worker put the data in the right place
-				url: 'https://call-distributor-dev.firebaseio.com/queue/tasks.json',
-				data: answerer
-			}).then(function successCallback(response) {
-				$log.debug(response.data);
+			//TODO accesss the queue with an injectable service instead of this garbage
+			newNumberSvc.new_answerer(answerer).then(function (data) {
+				$log.debug(data);
 				alert("Phone number saved!!");
-
-			}, function errorCallback(response) {
-				$log.error(response);
-			});
-
+			}, function (error) {
+				$log.error(error);
+				alert("Error: " + error.error);
+			})
 		}
 
 
